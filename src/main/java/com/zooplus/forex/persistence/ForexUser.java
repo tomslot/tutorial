fd20395e176131215b71c0cc07a87ff52147f66b
tomslot@gmail.com <mopstwon>
2016-04-11 16:38:08 +0200

    Using JPA for authentication

---
+++ src/main/java/com/zooplus/forex/persistence/ForexUser.java
@@ -0,0 +1,53 @@
+package com.zooplus.forex.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.beans.Transient;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+public class ForexUser {
+    private Long id;
+    private String login;
+    private String password;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+
+}
