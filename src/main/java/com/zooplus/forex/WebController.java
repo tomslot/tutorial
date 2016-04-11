bc373411d86de350bfbb885711007b96adc56083
tomslot@gmail.com <mopstwon>
2016-04-11 16:53:31 +0200

    displaying the name of logged in user

--- src/main/java/com/zooplus/forex/WebController.java
+++ src/main/java/com/zooplus/forex/WebController.java
@@ -1,5 +1,7 @@
 package com.zooplus.forex;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,9 @@ public class WebController {
     @RequestMapping("/")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("login", auth.getName());
+
         return "consult";
     }
 
