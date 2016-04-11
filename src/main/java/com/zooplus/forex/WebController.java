4a7cf3a9b78d512f51e9d90d58b4a067ed4ac69a
tomslot@gmail.com <mopstwon>
2016-04-11 22:43:46 +0200

    adding users using UI

--- src/main/java/com/zooplus/forex/WebController.java
+++ src/main/java/com/zooplus/forex/WebController.java
@@ -1,27 +1,58 @@
 package com.zooplus.forex;
 
+import com.zooplus.forex.persistence.ForexUser;
+import com.zooplus.forex.persistence.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.Collections;
 
 @Controller
 public class WebController {
+    @Autowired
+    UserRepository userRepository;
 
-    @RequestMapping("/")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @RequestMapping(value = "/")
+    public String calculate(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("login", auth.getName());
 
         return "consult";
     }
 
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String saveNewUser(@Valid ForexUser forexUser, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()){
+            return "register";
+        }
+
+        userRepository.save(Collections.singleton(forexUser));
+        model.addAttribute("login", forexUser.getLogin());
+
+        return "user_saved";
+    }
+
     @RequestMapping("/login")
-    public String login(){
+    public String login(@ModelAttribute(value = "message") String message, Model model){
         return "login";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String register(Model model){
+        ForexUser user = new ForexUser();
+        model.addAttribute("forexUser", user);
+        return "register";
     }
 
 }
--- src/main/java/com/zooplus/forex/persistence/ForexUser.java
+++ src/main/java/com/zooplus/forex/persistence/ForexUser.java
@@ -1,9 +1,9 @@
 package com.zooplus.forex.persistence;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -28,6 +28,9 @@ public class ForexUser {
         this.id = id;
     }
 
+    @NotNull
+    @Size(min = 3, max = 16)
+    @Column(unique = true)
     public String getLogin() {
         return login;
     }
@@ -36,6 +39,8 @@ public class ForexUser {
         this.login = login;
     }
 
+    @NotNull
+    @Size(min = 3, max = 16)
     public String getPassword() {
         return password;
     }
